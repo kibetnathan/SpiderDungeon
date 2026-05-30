d9444b97e9a0df7cef66902edc7a632afa8f41af
kibetnathan <kibetnat@gmail.com>
2026-05-30 19:43:17 +0300

    Initial commit

---
+++ assets/SpiderDungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="SpiderDungeon" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="Bundles/Terrain_alt/Dungeon_Tileset_at.png" width="512" height="512"/>
+</tileset>
