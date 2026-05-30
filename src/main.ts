1c81a82689ecbd36986545afd2109e06607abcf9
kibetnathan <kibetnat@gmail.com>
2026-05-31 00:36:11 +0300

    feat(movement): add diagonal movement with class based implementation of input handling

--- src/main.ts
+++ src/main.ts
@@ -8,31 +8,50 @@ canvas.style.background = "#dfdfca";
 
 const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 context.imageSmoothingEnabled = false;
-// Functions
-// Keystroke Event Listening Function
-const keystrokeListener = () => {
-  window.addEventListener("keydown", (e) => {
-    switch (e.key) {
-      case "w":
-        playerSprite.move(0, -7.5);
-        break;
-      case "a":
-        playerSprite.move(-7.5, 0);
-        break;
-      case "s":
-        playerSprite.move(0, 7.5);
-        break;
-      case "d":
-        playerSprite.move(7.5, 0);
-        break;
-      case " ": // Space Bar
-        break;
-      default:
-        console.log(e);
-    }
-  });
+// Helper Functions
+const handleInput = () => {
+  if (input.isPressed("w")) {
+    playerSprite.move(0, -playerSprite.speed);
+  }
+  if (input.isPressed("a")) {
+    playerSprite.move(-playerSprite.speed, 0);
+  }
+  if (input.isPressed("s")) {
+    playerSprite.move(0, playerSprite.speed);
+  }
+  if (input.isPressed("d")) {
+    playerSprite.move(playerSprite.speed, 0);
+  }
 };
 
+// Classes
+// Keystroke Event Controller
+
+class InputController {
+  keys: Record<string, boolean>;
+  constructor() {
+    // Object holding the keys
+    this.keys = {};
+
+    // Keydown listener
+    window.addEventListener("keydown", (e) => {
+      if (" " === e.key) {
+        e.preventDefault();
+        // Keep space for the game inputs
+      }
+      this.keys[e.key.toLowerCase()] = true;
+    });
+
+    // Keyup listener
+    window.addEventListener("keyup", (e) => {
+      this.keys[e.key.toLowerCase()] = false;
+    });
+  }
+
+  isPressed(key: string) {
+    return !!this.keys[key.toLowerCase()];
+  }
+}
 // Player Class
 class Sprite {
   height: number;
@@ -41,6 +60,7 @@ class Sprite {
   sprite: HTMLImageElement;
   xpos: number;
   ypos: number;
+  speed: number;
   // image takes in the image path
   public constructor(
     height: number,
@@ -56,6 +76,7 @@ class Sprite {
     this.sprite.src = image_src;
     this.xpos = xpos;
     this.ypos = ypos;
+    this.speed = 3;
   }
   move(dx: number, dy: number) {
     // move the sprite by incrementing the xpos/ypos by the params passed then redrawing the sprite
@@ -101,7 +122,7 @@ playerSprite.sprite.onload = () => {
 };
 // Loading map
 // Initialise window event listener
-keystrokeListener();
+const input = new InputController();
 const mapImage = new map(-120, -80, "../assets/public/untitled.png");
 
 // Animation loop
@@ -122,5 +143,6 @@ const animate = () => {
     playerSprite.width * 1.5,
     playerSprite.height * 1.5,
   );
+  handleInput();
 };
 mapImage.image.onload = () => animate();
